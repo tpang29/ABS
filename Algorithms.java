0e16410bdb8b55ad856d5dd74028f009ef7e1551
tpang29 <tpang29@gmail.com>
2020-04-30 09:40:09 -0400

    add insertionSortOptimized

--- Algorithms.java
+++ Algorithms.java
@@ -115,6 +115,51 @@ public class Algorithms
     {
         final String NAME = "insertion sort";
         PrintUtils.algorithm(true, NAME, array);
+        boolean swapped = false;
+        int pass = 0;
+
+        for (int i = 1; i < array.length; i++)
+        {   
+            pass = i;
+            PrintUtils.passHeader(pass, array);
+
+            for (int j = i; j > 0; j--)
+            {   
+                // utility variables for step summary 
+                swapped = false;
+                String compareStmt = PrintUtils.compare(array[j], array[j - 1]);
+                
+                // if element on the right is greater than adjacent element on the left
+                if (array[j] < array[j - 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    swapped = true;
+                }
+                
+                // Print summary for this step
+                String actionStmt = PrintUtils.action(swapped, array[j], array[j - 1]);
+                PrintUtils.step(compareStmt, actionStmt, array);
+            }
+
+            PrintUtils.passFooter(pass, array);
+        }
+
+        PrintUtils.algorithm(false, NAME, array);
+    }
+
+    /*
+        Insertion Sort algorithm, optimized
+
+        - terminate early if a comparison is made where no swap need occur
+        - this prevents us from making unnecessary comparisons to elements
+            we know to be <=
+    */
+    public static void insertionSortOptimized(int[] array)
+    {
+        final String NAME = "insertion sort optimized";
+        PrintUtils.algorithm(true, NAME, array);
 
         boolean swapped = false;
         int pass = 0;
@@ -143,6 +188,7 @@ public class Algorithms
                 String actionStmt = PrintUtils.action(swapped, array[j], array[j - 1]);
                 PrintUtils.step(compareStmt, actionStmt, array);
 
+                // Early termination if no swaps detected
                 if (!swapped)
                 {
                     break;
